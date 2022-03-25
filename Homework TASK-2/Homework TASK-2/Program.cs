a3065618b6f41b4c8d79b0654200355152b24670
Malik8008 <tu60prbs1@code.edu.az>
2022-03-25 08:04:09 +0400

    CheckFullName and CheckGroupNo completed

---
+++ Homework TASK-2/Homework TASK-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_TASK_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student();
+            
+
+            Console.WriteLine("Ad ve Soyad daxil edin");
+            string fn=Console.ReadLine();                       
+            Console.WriteLine("Group No daxil edin");
+            string gn=Console.ReadLine();
+            byte number = 21;
+            Console.WriteLine($"Telebenin yashi: {number}");
+
+            student.age = number;
+            student.CheckFullName = fn;
+            student.CheckGroupNo = gn;
+        }       
+    }
+}
---
+++ Homework TASK-2/Homework TASK-2/Student.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_TASK_2
+{
+    internal class Student
+    {
+        private string  _groupno;
+        private string _fullname;
+        public byte age;
+        public  string CheckGroupNo
+        {
+            get
+            {
+                return _groupno;
+            }
+            set
+            {
+                while (!CheckNo(value))
+                {
+                    Console.WriteLine("Duzgun Group no daxil edin.\n");
+                    value = Console.ReadLine();
+                }
+                Console.WriteLine("Group no qeyde alindi.\n");
+
+                _groupno = value;
+            }
+        }
+
+        public  string CheckFullName {
+            get
+            {
+                return _fullname;
+            }
+            set
+            {
+                while (!CheckFull(value))
+                {
+                    Console.WriteLine("Duzgun Ad ve Soyad qeyd edin.\n");
+                    value = Console.ReadLine();
+                }
+                Console.WriteLine("Ad ve Soyad qeyde alindi.\n");
+                _fullname = value;
+            }
+        }        
+        public static bool CheckNo(string groupno)
+        {
+            if (groupno.Length==4)
+            {
+                bool HasDigit = false;
+                bool HasUpper = false;
+                
+                foreach (char filter in groupno)
+                {
+                    if (char.IsDigit(filter))
+                    {
+                        HasDigit = true;
+                        continue;
+                    }
+                    if (char.IsUpper(filter))
+                    {
+                        HasUpper = true;                        
+                    }                    
+                }
+                bool result = HasUpper && HasDigit;
+                return result;
+            }            
+            return false;
+            
+        }
+        public static bool CheckFull(string fullname)
+        {
+            int count = 0;
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsUpper(fullname[i]) && i == 0)
+                {
+                    count++;
+                }
+                else if (char.IsWhiteSpace(fullname[i]))
+                {
+                    count++;
+                    if (char.IsUpper(fullname[i + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count == 3 ? true : false;
+        }
+    }
+}
