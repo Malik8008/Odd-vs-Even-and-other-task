b84c9561926646579f2e660036ea338c42740892
Malik8008 <tu60prbs1@code.edu.az>
2022-03-25 08:02:55 +0400

    Odd vs Even completed

---
+++ Odd vs Even Opp/Odd vs Even Opp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Odd_vs_Even_Opp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsOdd(12));
+            Console.WriteLine(IsEven(12));  
+        }
+        public static bool IsOdd(int num)
+        {
+            return num % 2 == 1;
+        }
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;  
+
+        }
+    }
+}
