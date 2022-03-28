8cbcf1fb5e00f59a85cfb51f625b0b385f8cc808
Malik8008 <tu60prbs1@code.edu.az>
2022-03-28 11:09:13 +0400

    BigWord completed

---
+++ Task-2/Task-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigR("Salam1"));
+        }
+
+        public static bool BigR(string check)
+        {
+            bool HasDigit = false;
+            bool HasUpper = false;
+            bool Haslower = false;
+
+            foreach (char letter in check)
+            {
+                if (char.IsDigit(letter))
+                {
+                    HasDigit = true;
+                    continue; 
+                }
+                if (char.IsUpper(letter))
+                {
+                    HasUpper = true;
+                    continue;
+                }
+                if (char.IsLower(letter))
+                {
+                    Haslower = true;                    
+                }               
+            }
+            bool result = HasDigit && HasUpper && Haslower;
+            return result;
+        }               
+    }
+}
