f3f8c4408d7b7c03f5d201a93f4939c3a03caf05
Malik8008 <tu60prbs1@code.edu.az>
2022-03-28 11:11:06 +0400

    FirstWord completed

---
+++ Task-3/Task-3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstWorld("Hello ey zalim dunya necesen?");
+        }
+
+        public static void FirstWorld(string sentence)
+        {            
+            Console.WriteLine(sentence.Substring(0,sentence.IndexOf(' ')));
+        }
+    }
+}
