923e3012e6d1e743eabf178dac99f72d67666b30
Malik8008 <tu60prbs1@code.edu.az>
2022-03-28 11:15:38 +0400

    AddArr Completed

---
+++ Task-5/Task-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {12,15,18,24,45 };
+            AddArr(ref array);
+        }
+
+        public static void AddArr( ref int [] array)
+        {
+            Array.Resize(ref array, array.Length+1);            
+            foreach (var add in array)
+            {
+                array[array.Length - 1] = 55;
+                Console.WriteLine(add);
+            }
+        }
+    }
+}
