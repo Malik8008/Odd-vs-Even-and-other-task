d30c73518f4cb09e6a933bcb82d35787c34b56ae
Malik8008 <tu60prbs1@code.edu.az>
2022-03-28 11:13:50 +0400

    TwoUpper Word completed

---
+++ Task-4/Task-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstBigWorld("Elon Musk"));
+        }
+
+        public static bool FirstBigWorld(string sentence)
+        {
+            int word=0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsUpper(sentence[i]) && i==0)
+                {
+                    word++;
+                }
+                else if (char.IsWhiteSpace(sentence[i]))
+                {
+                    word++;
+                    if (char.IsUpper(sentence[i + 1]))
+                    {
+                        word++;
+                    }
+                }
+            }
+            return word==3? true:false;
+        }
+    }
+}
